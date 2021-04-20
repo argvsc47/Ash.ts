26be4471ef4cea93fad82d74faedd8c56488c69c
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-04-20 16:24:01 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,6 @@ import { getConfig } from 'doge-config';
 const config = getConfig('ash.ts', {
 	port: 8000,
 	protocol: 'http',
-	urls: {
-		'/':''
-	},
 });
 
 export default config;
@@ -13,4 +10,3 @@ module.exports = config;
 
 export const port = config.__getNumber('port');
 export const protocol = config.__getString('protocol');
-export const urls = config.__getField('urls');
