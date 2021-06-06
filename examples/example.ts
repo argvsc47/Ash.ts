5690a6675efbe1b2e55c75a8de5d23490dd3d1fb
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-06-06 11:12:15 +0100

    Update example.ts

--- examples/example.ts
+++ examples/example.ts
@@ -1,7 +1,7 @@
 const ash = require('ash');
 var prof = new ash.Profiler();
 
-var server = new ash.Server(prof.getProfile('dev'));
+var server = new ash.asherver(prof.getProfile('dev'));
 server.router.route('/', function(request, response) {
     response.write("Hello, World!");
     response.end();
